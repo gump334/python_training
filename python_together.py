9f7640ce04f7139852c7c70589b708328fc9698d
blackcloudgeek <gumpfinest3341983@gmail.com>
2020-05-29 07:45:56 -0500

    created a python script to remove only files from any directory a user input

--- python_together.py
+++ python_together.py
@@ -1,3 +1,22 @@
-# hello coders
-print("now it's something here!")  # print functiion output the return
-print("let's write some python")
+#!/usr/bin/env python3
+
+# create a python script that will clean old files from your
+import os
+
+# User input for dir path and file name
+print("list the file path of dir that file exist in to be deleted")
+dir = input()
+print("enter the file name to be delete")
+file_name = input()
+home = os.chdir(dir)
+file_list = os.listdir()
+
+# check to see if content is a file or directory. if file is located then it will delete it. if dir then it will confirm that it is
+if os.path.isfile(file_name):
+    print("file exists, removing file!")
+    os.remove(file_name)
+elif os.path.isdir(file_name):
+    print("this is a directory")
+else:
+    print("file does not exist")
+
